f078b5ea72eaa7b3c78eee2bbfd88473e6e65fbe
mprusac5 <mprusac23@student.foi.hr>
2025-11-29 15:56:48 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
+
 export default defineConfig(({ mode }) => ({
+  base: "/brzo-auto-otkup-afe96e0d/", 
   server: {
     host: "::",
     port: 8080,
